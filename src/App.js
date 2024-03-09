50532d27c59c5e6b58c473550718ce1d0b327738
saurabh dongre <saurabhdongre237@gmail.com>
2024-03-09 15:43:32 +0530

    Update files

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import Textform from './components/Textform';
+import Alert from './components/Alert';
+// import Change from './components/Change';
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showalert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
+
+  const togglemode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showalert('Dark mode is Enabled', 'success');
+    } else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showalert('Light mode is Enabled', 'success');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+           <>
+              <Navbar title="Text Converter" mode={mode} togglemode={togglemode} />
+              <Alert alert={alert} />
+              <div className="container my-3">
+                <Textform heading="Enter the text to analyze below" mode={mode} showalert={showalert} />
+              </div>
+            </>
+        // {<Change />}
   );
 }
 
